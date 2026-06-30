bf083d27e99620353ee6ec86bd3718b30d8ebadf
Saniya Thakur <thakursaniya102@gmail.com>
2026-06-30 17:00:02 +0530

    Update settings.py

--- expense_tracker_project/settings.py
+++ expense_tracker_project/settings.py
@@ -33,7 +33,7 @@ CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
 CRISPY_TEMPLATE_PACK = "bootstrap5"
 
 LOGIN_URL = 'login'
-LOGIN_REDIRECT_URL = 'expense_list'
+LOGIN_REDIRECT_URL =  '/expenses/'
 LOGOUT_REDIRECT_URL = 'login'
 
 DEMO_USERNAME = 'demo'
